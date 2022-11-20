935b2ab633ebe8d1349304bcc6507e86453f2af7
r3m/romiw <gnabgib@users.noreply.github.com>
2022-11-20 14:03:32 -0700

    # Code cleanup

--- checksum/luhn/luhn.go
+++ checksum/luhn/luhn.go
@@ -6,6 +6,7 @@ package luhn
 // https://www.dcode.fr/luhn-algorithm
 
 
+// Generate a single digit (0-9) checksum for the given number
 func Checksum(i uint64) uint8 {
 	var ret= uint64(0)
 	var mul= uint64(2)
@@ -18,37 +19,3 @@ func Checksum(i uint64) uint8 {
 	}
 	return uint8((10-ret%10)%10)
 }
-
-// import "github.com/gnabgib/gnablib-go/checksum"
-
-// type digest uint8
-
-// // A new Hash8 for computing the Longitudinal redundancy check checksum
-// func New() checksum.Hash8 {
-// 	d := new(digest)
-// 	d.Reset()
-// 	return d
-// }
-
-// func (d *digest) update(p []byte) {
-// 	dig:=uint8(*d)
-// 	for i:=0;i<len(p);i++ {
-// 		dig+=p[i]
-// 	}
-// 	*d=digest(^dig+1)
-// }
-
-// func (d *digest) Write(p []byte) (n int, err error) {
-// 	d.update(p)
-// 	return len(p), nil
-// }
-
-// func (d *digest) Sum(in []byte) []byte { return append(in, byte(*d)) }
-
-// func (d *digest) Reset() { *d = 0 }
-
-// func (d *digest) Size() int { return 1 }
-
-// func (d *digest) BlockSize() int { return 1 }
-
-// func (d *digest) Sum8() uint8 { return byte(*d) }
